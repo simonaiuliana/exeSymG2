9fccff506825bfd1bf5a7d2def2e53988f0d1efc
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2024-10-07 14:16:11 +0200

    ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,21 +9,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('front/home.html.twig');
     }
 
     #[Route('/info', name: 'info')]
     public function info(): Response
     {
-        return $this->render('info.html.twig');
+        return $this->render('front/info.html.twig');
     }
 
     #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
-        return $this->render('contact.html.twig');
+        return $this->render('front/contact.html.twig');
     }
+
+    #[Route('/articles', name: 'articles')]
+    public function articles(): Response
+    {
+        return $this->render('admin/articles.html.twig');
+    }
+
 }
 
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-# on va charger le Repository (manager) de Section
 use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,28 +13,29 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils, SectionRepository $sectionRepository): Response
     {
-        // si on est déjà connecté et qu'on souhaite revenir sur login
-        if($this->getUser()) {
-            // on retourne sur l'accueil
-            return $this->redirectToRoute('coucou');
+        // Redirectare dacă utilizatorul este deja autentificat
+        if ($this->getUser()) {
+            return $this->redirectToRoute('coucou'); // Asigură-te că ruta 'coucou' este definită
         }
-        // get the login error if there is one
+
+        // Obține eroarea de autentificare dacă există
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
+        // Ultimul username introdus de utilizator
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
             'title' => "Connexion",
-            'sections' => $sectionRepository->findAll(),
+            'sections' => $sectionRepository->findAll(), // Asigură-te că ai o entitate Section
         ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
+        // Este gestionat de Symfony, nu trebuie implementat
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
